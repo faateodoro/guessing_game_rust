a2202a67401d760878c8b7435f389fd45b791251
Fabiano A. Teodoro <faateodoro@gmail.com>
2018-10-16 02:14:24 -0300

    Some lines writen.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// adiciona a biblioteca std::in no programa
+// senão, toda entrada de dados seria std::io::stdin()
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing the number!" );
+
+    println!("Please input your guess.");
+
+    // Cria a variavel mutavel guess e adiciona
+    // uma instancia vazia do metodo estático String
+    let mut guess = String::new();
+
+    // io::stdin() é uma função padrão de entrada de dados
+    // .read_line(&mut guess) entrada de dados com o teclado
+    // que passa o valor como referencia para guess.
+    // 
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed: {}", guess);
 }
